d01d8710efb0d7f3bf0377e3774bef372916ba06
Kiran Karadia <kiran.karadia@hudl.com>
2025-09-23 22:23:36 +0100

    Change sign characters for gitsigns. Enable blame and line number highlighting

--- init.lua
+++ init.lua
@@ -272,11 +272,25 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
+        add = { text = '█' },
+        change = { text = '█' },
+        delete = { text = '▃' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      numhl = true,
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_test_pos = 'eol',
       },
     },
   },
